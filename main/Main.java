69f01c8b7912bbccc6ec39310051c4935960922b
sophheehee <somar12@saddleback.edu>
2026-04-19 18:55:26 -0700

    moving files around and updates to main

--- main/Main.java
+++ main/Main.java
@@ -1,1 +1,32 @@
-//this is main for now
+//imports
+import java.util.Scanner;
+import java.util.Vector;
+
+
+public class Main {
+    public Main(){super();}
+    public static void main(String[] args){
+        // create new bank manager object
+        Bank bankManager = new Bank();
+
+        //import utilities
+        Scanner input = new Scanner(System.in);
+
+        // MAIN PROGRAM LOOP
+        int mainChoice = 0;
+        while(mainChoice ! = 3){
+            System.out.println();
+            System.out.println("Welcome to Saddleback Bank");
+
+            // Main Menu
+            System.out.println("---------------------------");
+            System.out.println("     Account Options ");
+            System.out.println("---------------------------");
+            System.out.println("1. Open New Account");
+            System.out.println("2. Manage Existing Account");
+            System.out.println("3. Exit ");
+
+
+        }
+    }
+}
